f505d90ed1c689c44cc69196b945a63cf7c1780a
Arthur <gamerjuanma9@gmail.com>
2026-04-11 07:29:55 -0300

    Avance del mini RPG de práctica

--- SEMANA 6 (Flet II)/dia44.py
+++ SEMANA 6 (Flet II)/dia44.py
@@ -1,6 +1,21 @@
 import flet as ft
 import asyncio
 
+# Simulación de base de datos (me gusta jugar con que ya estoy operando en backend jsjs):
+nombre_de_usuario = "Arthuritou007"
+contraseña = "chipilipi123"
+inventario = ["🗡️ Daga oxidada", "❤️ Pósima"]
+
+# Inventario: 
+items_visuales = []
+
+for nombre in inventario:
+    items_visuales.append(ft.Text(f"{nombre}", size=20, weight=ft.FontWeight.BOLD))
+
+mochila = ft.Column(spacing=10, controls=items_visuales)
+
+
+# Funciones para falicitar los textos:
 def crear_titulo(texto, color):
     return ft.Text(
         f"{texto}",
@@ -36,30 +51,70 @@ def crear_textfield(label, color, color2):
     )
 
 
-# Segunda pestaña:
+# Pestaña del bosque:
 def mostrar_juego(page):
     page.clean()
-    texto1_p2 = crear_texto("Acabas de despertar... Tu cabeza da vueltas y no sabes en dónde estás.", ft.Colors.WHITE)
-    texto2_p2 = crear_texto("Ves en frente de ti un camino entre los árboles que se extiende hacia la profundidad del bosque", ft.Colors.WHITE)
-    texto3_p2 = crear_texto("¿", ft.Colors.WHITE)
-    page.add(texto1_p2)
-    page.add(texto2_p2)
-    page.add(texto3_p2)
+
+    def abrir_mochila(e):
+        funcion_inventario(page)
+
+    async def quedarse(e):
+        await quedarse_en_el_lugar(page)
+
+    texto1_bosque = crear_texto("Acabas de despertar... Tu cabeza da vueltas y no sabes en dónde estás.", ft.Colors.WHITE)
+    texto2_bosque = crear_texto("Ves en frente de ti un camino entre los árboles que se extiende hacia la profundidad del bosque.", ft.Colors.WHITE)
+
+    boton_bosque1 = ft.ElevatedButton("Explorar", align=ft.Alignment.CENTER, width=250, height=60)
+    boton_bosque2 = ft.ElevatedButton("Quedarse en el lugar", align=ft.Alignment.CENTER, width=250, height=60, on_click=quedarse)
+    boton_bosque3 = ft.ElevatedButton("Inventario", align=ft.Alignment.CENTER, width=250, height=60, on_click=abrir_mochila)
+
+    fila_bosque = ft.Row(controls=[boton_bosque1, boton_bosque2], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
+
+    page.add(texto1_bosque, texto2_bosque, fila_bosque, boton_bosque3)
     page.update()
 
-# Simulación de base de datos (me gusta jugar con que ya estoy operando en backend jsjs):
+# Opción "Inventario"
+def funcion_inventario(page):
+    page.clean()
 
-nombre_de_usuario = "Arthuritou007"
-contraseña = "chipilipi123"
-inventario = ["🗡️ Daga oxidada", "❤️ Pósima"]
+    def cerrar_inventario(e):
+        page.clean()
+        mostrar_juego(page)
+
+    texto = crear_titulo("Inventario:", ft.Colors.BROWN)
+    cerrar_mochila = ft.ElevatedButton("Cerrar mochila", align=ft.Alignment.CENTER, width=250, height=60, on_click=cerrar_inventario)
+    contenedor_mochila = ft.Container(
+        content=mochila,
+        align=ft.Alignment.CENTER,
+        bgcolor=ft.Colors.BROWN_900,
+        padding=20,
+        border_radius=10
+        )
+
+    page.add(texto, contenedor_mochila, cerrar_mochila)
+
+# Opción "Quedarse en el lugar"
+async def quedarse_en_el_lugar(page):
+    page.clean()
+
+    texto1_quedarse = crear_titulo("No te has movido ni un pelo...", ft.Colors.WHITE)
+    page.add(texto1_quedarse)
+
+    await asyncio.sleep(3)
+
+    mostrar_juego(page)
+
 
 def main(page):
+
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     async def login(e):
         
         if ingresar_contraseña_p1.value == contraseña and ingresar_usuario_p1.value == nombre_de_usuario:
             page.clean()
-            page.add(crear_titulo(f"¡Bienvenido {nombre_de_usuario}!", ft.Colors.WHITE))
+            page.add(crear_titulo(f"¡Bienvenido, {nombre_de_usuario}!", ft.Colors.WHITE))
             page.add(crear_subtitulo("Redirigiendo...", ft.Colors.WHITE))
             page.update()
 
@@ -71,13 +126,6 @@ def main(page):
             alerta_p1.value = "Los datos ingresados son incorrectos. Intente nuevamente"
             page.update()
 
-    items_visuales = []
-
-    for nombre in inventario:
-        items_visuales.append(ft.Text(f"{nombre}", size=20))
-
-    mochila = ft.ListView(expand=True, spacing=10, controls=items_visuales)
-
     espacio_vacio = ft.Container(height=50)
 
     titulo_p1 = crear_titulo("--=== ¡Bienvenido a LK Dungeon Quest! ===--", ft.Colors.BLUE_900)
@@ -87,7 +135,7 @@ def main(page):
     boton_p1 = ft.ElevatedButton("Iniciar sesión", align=ft.Alignment.CENTER, width=250, height=60, on_click=login)
     alerta_p1 = crear_subtitulo(" ", ft.Colors.BLUE_900)
 
-    registro = ft.Row(controls=[ingresar_usuario_p1, ingresar_contraseña_p1], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
+    registro = ft.Row(controls=[ingresar_usuario_p1, ingresar_contraseña_p1], alignment=ft.MainAxisAlignment.CENTER)
 
 
     page.add(titulo_p1, espacio_vacio, subtitulo_p1, registro, espacio_vacio, boton_p1, alerta_p1)
