e891d90f82563f466ffad909c554cc664cde3713
Arthur <gamerjuanma9@gmail.com>
2026-04-12 07:56:51 -0300

    Update del rpg de práctica

--- SEMANA 6 (Flet II)/dia44.py
+++ SEMANA 6 (Flet II)/dia44.py
@@ -5,6 +5,13 @@ import asyncio
 nombre_de_usuario = "Arthuritou007"
 contraseña = "chipilipi123"
 inventario = ["🗡️ Daga oxidada", "❤️ Pósima"]
+oro = 200
+tienda = [
+    {"nombre": "Espada Berserker", "precio": 1000},
+    {"nombre": "Pócima", "precio": 50},
+    {"nombre": "Pan", "precio": 100},
+    {"nombre": "Mapa Misterioso", "precio": 10}
+]
 
 # Inventario: 
 items_visuales = []
@@ -37,7 +44,7 @@ def crear_subtitulo(texto, color):
 def crear_texto(texto, color):
     return ft.Text(
         f"{texto}",
-        size=15,
+        size=20,
         color=color,
         weight=ft.FontWeight.BOLD,
         align=ft.Alignment.CENTER
@@ -50,6 +57,7 @@ def crear_textfield(label, color, color2):
         bgcolor=color2
     )
 
+espacio_vacio = ft.Container(height=50)
 
 # Pestaña del bosque:
 def mostrar_juego(page):
@@ -61,12 +69,15 @@ def mostrar_juego(page):
     async def quedarse(e):
         await quedarse_en_el_lugar(page)
 
+    async def acción_explorar(e):
+        await explorar(page)
+
     texto1_bosque = crear_texto("Acabas de despertar... Tu cabeza da vueltas y no sabes en dónde estás.", ft.Colors.WHITE)
     texto2_bosque = crear_texto("Ves en frente de ti un camino entre los árboles que se extiende hacia la profundidad del bosque.", ft.Colors.WHITE)
 
-    boton_bosque1 = ft.ElevatedButton("Explorar", align=ft.Alignment.CENTER, width=250, height=60)
-    boton_bosque2 = ft.ElevatedButton("Quedarse en el lugar", align=ft.Alignment.CENTER, width=250, height=60, on_click=quedarse)
-    boton_bosque3 = ft.ElevatedButton("Inventario", align=ft.Alignment.CENTER, width=250, height=60, on_click=abrir_mochila)
+    boton_bosque1 = ft.Button("Explorar", align=ft.Alignment.CENTER, width=250, height=60, on_click=acción_explorar)
+    boton_bosque2 = ft.Button("Quedarse en el lugar", align=ft.Alignment.CENTER, width=250, height=60, on_click=quedarse)
+    boton_bosque3 = ft.Button("Inventario", align=ft.Alignment.CENTER, width=250, height=60, on_click=abrir_mochila)
 
     fila_bosque = ft.Row(controls=[boton_bosque1, boton_bosque2], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
 
@@ -82,7 +93,7 @@ def funcion_inventario(page):
         mostrar_juego(page)
 
     texto = crear_titulo("Inventario:", ft.Colors.BROWN)
-    cerrar_mochila = ft.ElevatedButton("Cerrar mochila", align=ft.Alignment.CENTER, width=250, height=60, on_click=cerrar_inventario)
+    cerrar_mochila = ft.Button("Cerrar mochila", align=ft.Alignment.CENTER, width=250, height=60, on_click=cerrar_inventario)
     contenedor_mochila = ft.Container(
         content=mochila,
         align=ft.Alignment.CENTER,
@@ -103,6 +114,41 @@ async def quedarse_en_el_lugar(page):
     await asyncio.sleep(3)
 
     mostrar_juego(page)
+
+# Opción "Explorar"
+async def explorar(page):
+    page.clean()
+
+    texto1_explorar = crear_subtitulo("Has caminado un largo sendero y encontraste una tienda", ft.Colors.WHITE)
+    texto2_explorar = crear_subtitulo("Entras a la tienda y un hombre encapuchado se acerca a ti y por", ft.Colors.WHITE)
+    texto3_explorar = crear_subtitulo("arte de magia hace aparecer 4 objetos encima del mostrador", ft.Colors.WHITE)
+    texto4_explorar = crear_subtitulo('"Escoge sabiamente", te susurra entre risas', ft.Colors.WHITE)
+
+    espada_berserker1 = crear_texto(f"{tienda[0]["nombre"]}", color=ft.Colors.BLUE_900)
+    espada_berserker2 = ft.Button(f"COMPRAR (${tienda[0]["precio"]})", bgcolor=ft.Colors.BLUE_900, width=250, height=60)
+    espada_serberker3 = ft.Column(controls=[espada_berserker1, espada_berserker2], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
+    pocima1 = crear_texto(f"{tienda[1]["nombre"]}", color=ft.Colors.BLUE_900)
+    pocima2 = ft.Button(f"COMPRAR (${tienda[1]["precio"]})", bgcolor=ft.Colors.BLUE_900, width=250, height=60)
+    pocima3 = ft.Column(controls=[pocima1, pocima2], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
+    primera_fila = ft.Row(controls=[espada_serberker3, pocima3], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
+
+    pan1 = crear_texto(f"{tienda[2]["nombre"]}", color=ft.Colors.BLUE_900)
+    pan2 = ft.Button(f"COMPRAR (${tienda[2]["precio"]})", bgcolor=ft.Colors.BLUE_900, width=250, height=60)
+    pan3 = ft.Column(controls=[pan1, pan2], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
+    mapa_misterioso1 = crear_texto(f"{tienda[3]["nombre"]}", color=ft.Colors.BLUE_900)
+    mapa_misterioso2 = ft.Button(f"COMPRAR (${tienda[3]["precio"]})", bgcolor=ft.Colors.BLUE_900, width=250, height=60)
+    mapa_misterioso3 = ft.Column(controls=[mapa_misterioso1, mapa_misterioso2], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
+    segunda_fila = ft.Row(controls=[pan3, mapa_misterioso3], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
+
+    oro_disponible = crear_texto(f"{str(oro)}", ft.Colors.YELLOW)
+    oro_disponible2 = crear_texto(f"Oro: ", ft.Colors.WHITE)
+    total_oro = ft.Row(controls=[oro_disponible2, oro_disponible])
+
+    page.add(texto1_explorar, texto2_explorar, texto3_explorar, texto4_explorar, primera_fila, segunda_fila, total_oro)
 
 
 def main(page):
@@ -132,7 +178,7 @@ def main(page):
     subtitulo_p1 = crear_subtitulo("Para continuar, debe iniciar sesión", ft.Colors.BLUE_900)
     ingresar_usuario_p1 = crear_textfield("Ingrese aquí su nombre de usuario", ft.Colors.BLUE, ft.Colors.BLUE_900)
     ingresar_contraseña_p1 = crear_textfield("Ingrese aquí su contraseña", ft.Colors.BLUE, ft.Colors.BLUE_900)
-    boton_p1 = ft.ElevatedButton("Iniciar sesión", align=ft.Alignment.CENTER, width=250, height=60, on_click=login)
+    boton_p1 = ft.Button("Iniciar sesión", align=ft.Alignment.CENTER, width=250, height=60, on_click=login)
     alerta_p1 = crear_subtitulo(" ", ft.Colors.BLUE_900)
 
     registro = ft.Row(controls=[ingresar_usuario_p1, ingresar_contraseña_p1], alignment=ft.MainAxisAlignment.CENTER)
