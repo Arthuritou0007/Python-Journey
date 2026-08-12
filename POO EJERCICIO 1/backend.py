98eb273b086ddbaeb51cb3b47f45c0add0b49ccf
Arthur <gamerjuanma9@gmail.com>
2026-08-12 15:49:53 -0300

    POO

--- POO EJERCICIO 1/backend.py
+++ POO EJERCICIO 1/backend.py
@@ -1,5 +1,5 @@
 import json
 
 def guardar_datos(datos):
-    with open(f"DATABASE/{datos['nombre']}.json", "w", encoding="utf-8") as archivo:
+    with open(f"POO EJERCICIO 1/DATABASE/{datos['nombre']}.json", "w", encoding="utf-8") as archivo:
         json.dump(datos, archivo, indent=4, ensure_ascii=False)
