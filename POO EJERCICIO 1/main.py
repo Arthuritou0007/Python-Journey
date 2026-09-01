5d2e70dc795c779ab25a2ab1f2030974d422f1c6
Arthur <gamerjuanma9@gmail.com>
2026-09-01 12:09:35 -0300

    POO

--- POO EJERCICIO 1/main.py
+++ POO EJERCICIO 1/main.py
@@ -1,8 +1,54 @@
 from models import Mascota
 from backend import guardar_datos
 
-mascota_1 = Mascota("Bobby", "Perro", "Pedro", "Vómito")
+print("¡Hola! Bienvenido al menú interactivo")
+while True:
+    print("Escribe en la caja de texto la opción que desees:")
+    print("[1] Ingresar a la base de datos")
+    print("[2] Salir del menú")
+    try:
+        opcion = int(input("Ingresar opción:"))
+        if opcion == 1:
+            print("Abriendo base de datos...")
+            while True:
+                print("Ha entrado a la base de datos.")
+                print("[1] Registrar una nueva mascota")
+                print("[2] Ver mascotas registradas")
+                try:
+                    opcion = int(input("Ingresar opción:"))
+                    if opcion == 1:
+                        nombre = input("Ingrese el nombre de la mascota:")
+                        animal = input("Ingrese qué animal es:")
+                        dueño = input("Ingrese el nombre del dueño:")
+                        motivo = input("Ingrese el motivo de la consulta:")
 
-datos_para_guardar = mascota_1.to_dict()
+                        mascota = Mascota(nombre, animal, dueño, motivo)
+                        guardar_datos(mascota.to_dict())
 
-guardar_datos(datos_para_guardar)
+                        print("¡La mascota ha sido registrada con éxito!")
+                        print("¿Desea registrar otra mascota?")
+                        print("[1] Sí")
+                        print("[2] No (Salir)")
+                        try:
+                            opcion = int(input("Ingresar opción:"))
+
+                            if opcion == 1:
+                                continue
+                            if opcion == 2:
+                                break
+                            else:
+                                print("¡Ingrese un dato válido!")
+                        except ValueError:
+                            print("¡Ingrese un dato válido!")
+                    if opcion == 2:
+                        print("Mostrando base de datos...")
+                        break
+                except ValueError:
+                    print("¡Ingrese un dato válido!")
+        if opcion == 2:
+            print("¡Hasta luego!")
+            break
+        else:
+            print("¡Ingrese un dato válido!")
+    except ValueError:
+        print("¡Ingrese un dato válido!")
